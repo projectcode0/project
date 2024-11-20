50257d67480c18fea700ebbd2b899937164c8eab
projectcode0 <tempmail3382@gmail.com>
2024-11-20 20:21:42 +0530

    added dashboards files

--- restaurant-dashboard.js
+++ restaurant-dashboard.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const homeBtn = document.getElementById("home-btn");
     const logoutBtn = document.getElementById("logout-btn");
   
-    // Redirect to Customer Dashboard when Home is clicked
+    // Redirect to Restaurant Dashboard when Home is clicked
     homeBtn.addEventListener("click", () => {
-      window.location.href = "customer-dashboard.html";
+      window.location.href = "restaurant-dashboard.html";
     });
   
     // Logout and redirect to login page
     logoutBtn.addEventListener("click", () => {
       // Clear session or local storage
-      localStorage.removeItem("customerLoggedIn");
-      sessionStorage.removeItem("customerLoggedIn");
+      localStorage.removeItem("restaurantLoggedIn");
+      sessionStorage.removeItem("restaurantLoggedIn");
   
       // Redirect to the index page
       window.location.href = "index.html";
